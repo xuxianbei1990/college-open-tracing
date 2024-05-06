21ad8cb99b5f6dff7c738e1cf2b9e82a933b93fa
xuxianbei1990 <34258324+xuxianbei1990@users.noreply.github.com>
2024-05-06 18:43:40 +0800

    测试

--- order/src/main/java/college/order/controller/SampleController.java
+++ order/src/main/java/college/order/controller/SampleController.java
@@ -1,5 +1,6 @@
 package college.order.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RequestMapping("order/sample")
 @RestController
+@Slf4j
 public class SampleController {
 
     @GetMapping("hello")
